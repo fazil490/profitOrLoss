4b7c121c2c2d9f48f50e4422aa8b11e80d91d652
Fazil <mdif4900@gmail.com>
2022-06-23 12:36:14 +0530

    formula updated

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ function showOutput(message) {
 
 function calculateProfitOrLoss(initial, quantity, current) {
     if(initial>0 && quantity>0 && current>0) {
-        if (initial > current) {
-            var loss = (initial - current) * quantity;
-            var lossPercentage = ((loss / initial) * 100).toFixed(2);
-            showOutput("Oh oh! the loss is " + loss + " and the loss percent is  " + loss + "%");
-        } else if (current > initial) {
-            var profit = (current - initial) * quantity;
+        if (initial*quantity > current) {
+            var loss = (initial*quantity - current);
+            var lossPercentage = ((loss / (initial*quantity)) * 100).toFixed(2);
+            showOutput("Oh oh! the loss is " + loss + " and the loss percent is  " + lossPercentage + "%");
+        } else if (current > initial*quantity) {
+            var profit = (current - initial*quantity) ;
             var profitPercentage = ((profit / current) * 100).toFixed(2);
             showOutput("Hey! the profit is " + profit + " and the Profit percent is " + profitPercentage + "%")
         } else {
